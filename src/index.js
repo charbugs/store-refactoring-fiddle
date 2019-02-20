301d2736756c39d84d669228b85955b856db2582
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-02-20 12:32:47 +0100

    --

--- src/index.js
+++ src/index.js
@@ -8,30 +8,42 @@ import * as actions from './actions';
 async function test() {
   let result
 
-  /* fetch new manifest */
+  /*
+  * fetch new manifest
+  */
   const manifestUrl = 'https://iiif.harvardartmuseums.org/manifests/object/299843'
   result = await store.dispatch(actions.fetchManifest(manifestUrl))
   const manifestId = result.id
 
-  /* create new window */
+  /*
+  * create new window
+  */
   result = store.dispatch(actions.addWindow({
     canvasIndex: 0, collectionIndex: 0, manifestId: null
   }))
   const windowId = result.id
 
-  /* set reference to manifest in window */
+  /*
+  * set reference to manifest in window
+  */
   store.dispatch(actions.updateWindow(windowId, { manifestId }))
 
-  /* increment canvas index in window */
+  /*
+  * increment canvas index in window
+  */
   store.dispatch(actions.incrementCanvasIndex(windowId))
 
-  /* create companion window for window
-     triggers window reducer AND compantion reducer */
+  /*
+  * create companion window for window
+  * triggers window reducer AND compantion reducer
+  */
   store.dispatch(actions.createWindowCompanion(windowId, {
     content: 'info', position: 'right'
   }))
 
-  /* close window (remove window and corresponding companion windows) */
+  /*
+  * close window (remove window and corresponding companion windows)
+  */
   store.dispatch(actions.closeWindow(windowId))
 }
 
